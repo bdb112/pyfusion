Fix introduced problem in save_to_local, save_compress (cumsum and Nans) add units acquisition/base.py restore omitted sgn feature in newload, and insert error if it is in the wrong place. Basic Langmuir fitting (using amoeba) works, need to automate it.

pyfusion/data/save_compress.py
@@ -239,6 +239,9 @@ def discretise_signal(timebase=None, signal=None, parent_element=array(0),
     """
     from numpy import remainder, mod, min, max, \
         diff, mean, unique, append
+    from pyfusion.debug_ import debug_
+
+    debug_(pyfusion.DEBUG,1, key='discretise_signal')
 
     dat=discretise_array(signal,eps=eps,verbose=verbose, delta_encode=delta_encode_signal)
 #    signalexpr=str("signal=%g+rawsignal*%g" % (dat['minarr'], dat['deltar']))
@@ -308,13 +311,22 @@ def newload(filename, verbose=verbose):
         return(dic)  # quick, minimal return
 
     if verbose>2: print(' contains %s' % dic.files)
-    signalexpr=dic['signalexpr']
-    timebaseexpr=dic['timebaseexpr']
-# savez saves ARRAYS always, so have to turn array back into scalar    
-    exec(signalexpr.tolist())
-    exec(timebaseexpr.tolist())
+    # savez saves ARRAYS always, so have to turn array back into scalar    
+    signalexpr=dic['signalexpr'].tolist()
+    timebaseexpr=dic['timebaseexpr'].tolist()
+    # fixup for files written with np.nan removal and and cumsum
+    if ('cumsum' in timebaseexpr) and ('np.nan' in timebaseexpr):
+        print('!!!!!!!!!!!! fixup of nans with cumsum !!!!!!!!!!!!!!!!!!')
+        timebaseexpr = timebaseexpr.replace("timebase=",
+                             "temp=").replace("*2e-06","\ntimebase=temp*2e-06")
+        timebaseexpr = timebaseexpr.replace("== dic['rawtimebase']","== temp")
+
+    exec(signalexpr)
+    exec(timebaseexpr)
     retdic = {"signal":signal, "timebase":timebase, "parent_element":
-              dic['parent_element'], "params": dic['params'].tolist()}
+              dic['parent_element']}
+
+    if 'params' in dic: retdic.update({"params": dic['params'].tolist()})
     return(retdic)
 
     # return({"signal":signal, "timebase":timebase, "parent_element": dic['parent_element']})

pyfusion/examples/save_to_local.py
@@ -35,6 +35,7 @@ from pyfusion.data.save_compress import discretise_signal as savez_new
 import pyfusion.utils
 import numpy as np
 import os
+from pyfusion.debug_ import debug_
 
 from pyfusion.utils import process_cmd_line_args
 
@@ -59,9 +60,6 @@ exec(_var_defaults)
 exec(process_cmd_line_args())
 
 bads = []
-if 'W7' in diag_name and 'LP' in diag_name:
-    print('override encode time')
-    save_kwargs={"delta_encode_time":False}
 
 #s = pyfusion.get_shot(shot_number)
 #s.load_diag(diag_name, savelocal=True, ignorelocal=(compress_local==None), downsample=True)
@@ -90,6 +88,10 @@ else:
     diag_list = diag_name
     
 for diag in diag_list:
+    if 'W7' in diag and 'LP' in diag:
+        print('override encode time')
+        save_kwargs={"delta_encode_time":False}
+
     diag = prefix+diag
     for shot_number in shot_list:
         dev = pyfusion.getDevice(dev_name)
@@ -142,6 +144,7 @@ for diag in diag_list:
                     if os.path.isfile(localfilename) and not overwrite_local:
                         raise IOError('file {f} exists'.format(f=localfilename))
 
+                    debug_(pyfusion.DEBUG,1, key='save_to_local')
                     savez_new(signal=signal, timebase=tb, filename=localfilename, 
                               params = np.array(params),
                               verbose=pyfusion.VERBOSE, **save_kwargs)
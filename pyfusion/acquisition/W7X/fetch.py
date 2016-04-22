LHD problem fixed in restoring coordinates coding (incl W7X), transforms not properly implemented yet process_swept_Langmuir is more convenient to use (incl auto load and save), sign change of resid, save maxits, actual_params saved examples/modify_cfg.py is a script to add/modify pyfusion.cfg   (presently coordinates) Some gas controls in pyfusion.cfg start to convert mini_summary pyfusion.DBG() instead of pyfusion.DEBUG if a purely numeric value is   needed (e.g. in > or < tests).  This avoids unwanted debugger breaks when a   text key is used. fixed problem in LHD data access due to exception in LHDConvenience function.  (output_coords)

pyfusion/acquisition/W7X/fetch.py
@@ -60,6 +60,7 @@ def myhist(X):
 
 def regenerate_dim(x):
     """ assume x in ns since epoch from the current time """
+    msg = None  # msg allows us to see which shot/diag was at fault
     diffs = np.diff(x)
     # bincount needs a positive input and needs an array with N elts where N is the largest number input
     small = (diffs > 0) & (diffs < 1000000)
@@ -84,14 +85,14 @@ def regenerate_dim(x):
     x01111[0] = 0
     errcnt = np.sum(bigcounts) + np.sum(np.sort(counts)[::-1][1:])
     if errcnt>0 or (pyfusion.VERBOSE > 0): 
-        print('** repaired length of {l:,}, dtns={dtns:,}, {e} erroneous utcs'
+        msg = str('** repaired length of {l:,}, dtns={dtns:,}, {e} erroneous utcs'
               .format(l=len(x01111), dtns=dtns, e=errcnt))
 
     fixedx = np.cumsum(x01111)*dtns
     wbad = np.where((x - fixedx)>1e8)[0]
     fixedx[wbad] = np.nan
     debug_(pyfusion.DEBUG, 3, key="repair", msg="repair of W7-X scrambled Langmuir timebase") 
-    return(fixedx)
+    return(fixedx, msg)
     
 
 class W7XDataFetcher(BaseDataFetcher):
@@ -167,7 +168,10 @@ class W7XDataFetcher(BaseDataFetcher):
         elif self.repair == 1:
             dim = np.clip(dim, 0, 1e99)
         elif self.repair == 2:
-            dim = regenerate_dim(dim)
+            dim, msg = regenerate_dim(dim)
+            if msg is not None:
+                print('shot {s}, {c}: {m}'
+                      .format(s=self.shot, c=self.config_name, m=msg))
         else:
             raise ValueError('repair value of {r} not understood'.format(r=self.repair))
 

pyfusion/data/process_swept_Langmuir.py
@@ -12,7 +12,7 @@ This version assumes that all required sweep data are in a multi-channel diagnos
     from pyfusion.data.DA_datamining import Masked_DA, DA
     da=DA('20160310_9_L57',load=1)  # just to be sure it is a good DA
     da.masked=Masked_DA(['Te','I0','Vp'], DA=da)
-    da.da['mask']=(-da['resid']/abs(da['I0'])<.35) & (da['nits']<100)
+    da.da['mask']=(da['resid']/abs(da['I0'])<.35) & (da['nits']<100)
     clf();plot(da.masked['Te']);ylim(0,100)
 
 
@@ -92,17 +92,19 @@ class LPfitter():
 
         var = [Te, Vp, I0]
         scale = np.array([Te, Te/3, I0])
-        fit_results = amoeba.amoeba(var, scale, self.error_fun, itmax=maxits,
-                                    data=dict(v=self.v, i=self.i))
+        fit_results = list(amoeba.amoeba(var, scale, self.error_fun, itmax=maxits,
+                                    data=dict(v=self.v, i=self.i)))
         Te, Vp, I0 = fit_results[0]
+        fit_results[1] = -fit_results[1]  # change residual back to positive
         residual, mits = fit_results[1:3]
+        fit_results.append(maxits)
         if plot > 1:
             plt.scatter(self.v, self.i, c='r', s=80,
                         alpha=min(1, 4/np.sqrt(len(self.v))))
 
             self.plotchar(self.v, Te, Vp, I0, linewidth=4)
             plt.title('Te = {Te:.1f}eV, Vp = {Vp:.1f}, Isat = {I0:.3g}, resid = {r:.2e} {mits} its '
-                      .format(Te=Te, Vp=Vp, I0=I0, r=-residual, mits=mits))
+                      .format(Te=Te, Vp=Vp, I0=I0, r=residual, mits=mits))
             plt.show(block=0)
 
         return(fit_results)
@@ -270,12 +272,12 @@ class Langmuir_data():
             v = v_seg.signal[self.vlookup[self.vassoc[c]]]
             i = i_seg.signal[c]
             im = m_seg.signal[c]
-            [Te, ne, I0], resid, nits = self.fit_swept_Langmuir_seg_chan(im, i, v, i_seg, channame=chan.config_name, clipfact=clipfact, plot=plot)
+            [Te, ne, I0], resid, nits, maxits = self.fit_swept_Langmuir_seg_chan(im, i, v, i_seg, channame=chan.config_name, clipfact=clipfact, plot=plot)
             t_mid = np.average(i_seg.timebase)
-            res.append([t_mid, Te, ne, I0, resid, nits])
+            res.append([t_mid, Te, ne, I0, resid, nits, maxits])
         return(res)
     
-    def fit_swept_Langmuir_seg_chan(self, m_sig, i_sig, v_sig, i_segds, channame, clipfact=5, plot=None):
+    def fit_swept_Langmuir_seg_chan(self, m_sig, i_sig, v_sig, i_segds, channame, maxits=100, clipfact=5, plot=None):
         """Lowish level routine - takes care of i clipping and but not
         leakage or restoring sweep.
 
@@ -284,24 +286,24 @@ class Langmuir_data():
         # Note that we check the RAW current for clipping
         segtb = i_segds.timebase
         v, i, im = v_sig.copy(), i_sig.copy(), m_sig.copy()     # for fit
-        #v_n, i_n, im_n = v * np.nan, i * np.nan, im * np.nan,   # for nan plots
+        # v_n, i_n, im_n = v * np.nan, i * np.nan, im * np.nan,   # for nan plots
 
         good = find_clipped([v, im], clipfact=clipfact)
         wg = np.where(good)[0]
         if (~good).any():  # if any bad
             if len(wg) < 0.3 * len(v):  # was (~good).all():
                 print('suppressing all!*** {a}'.format(a=len(good)))
-                return([3*[None],None,None])
-            elif self.verbose>0 or len(wg) < 0.9 * len(v):
+                return([3*[None], None, None, None])
+            elif self.verbose > 0 or len(wg) < 0.9 * len(v):
                 print('suppress clipped {b}/{a}'
                       .format(b=len(np.where(~good)[0]), a=len(good)))
 
         # [[Te, Vp, I0], res, its]
         if plot is None:
             # plot in detail only if there are 20 figures or less
-            plot=self.plot + 2*((len(self.segs)*len(self.imeasfull.channels))< 8)
+            plot = self.plot + 2*((len(self.segs)*len(self.imeasfull.channels)) < 8)
         if plot >= 2:
-            interval = str('{t}'.format(t=[round(segtb[0],6), round(segtb[-1],6)]))
+            interval = str('{t}'.format(t=[round(segtb[0], 6), round(segtb[-1], 6)]))
             fig, axs = plt.subplots(nrows=1 + (plot >= 3), ncols=1, squeeze=0,
                                     num='{i} {c}'.format(i=interval, c=channame))
             if plot >= 3:
@@ -317,7 +319,7 @@ class Langmuir_data():
                          .format(tr=interval, shot=self.shot, d=self.i_diag, c=channame))
 
         #res = LPfit(v, i, plot=(debug > 1),maxits=100)
-        self.fitter = LPfitter(i=i[wg], v=v[wg], maxits=100, plot=plot, parent = self)
+        self.fitter = LPfitter(i=i[wg], v=v[wg], maxits=maxits, plot=plot, parent = self)
         return(self.fitter.fit(plot=plot))
 
     def write_DA(self, filename):
@@ -333,12 +335,12 @@ class Langmuir_data():
         dd['progId'][:] = self.shot[1]
         dd['shot'][:] = self.shot[1] + 1000*self.shot[0]
 
-        for key in ['nits']:
+        for key in ['nits','maxits']:
             dd[key] = np.zeros([nt,nc], dtype=np.uint16)
 
         # make all the f32 arrays - note - ne is just IO for now - fixed below
-        for (ind, key) in [(0, 't_mid'), (1, 'Te'), (2, 'Vp'),
-                           (3, 'I0'), (4, 'resid'), (5, 'nits'), (3, 'ne')]:
+        for (ind, key) in [(0, 't_mid'), (1, 'Te'), (2, 'Vp'), (3, 'I0'), 
+                           (4, 'resid'), (5, 'nits'), (6, 'maxits'), (3, 'ne')]:
             if key not in dd:
                 dd[key] = np.zeros([nt, nc], dtype=np.float32)
             dd[key][:] = res[:, :, ind]
@@ -347,13 +349,14 @@ class Langmuir_data():
         dd['t_mid'] = dd['t_mid'][:, 0]
         dd['info'] = dict(params=self.actual_params,
                           coords=self.coords,
+                          shotdata=dict(shot=[self.shot], utc_ns=[self.imeas.utc[0]]),
                           channels=[chn.replace(self.dev.name, '')
                                     .replace('_I', '')
                                     for chn in self.ichans])
         da = DA(dd)
         da.masked = Masked_DA(['Te', 'I0', 'Vp', 'ne'], DA=da)
-        #  da.da['mask']=(-da['resid']/abs(da['I0']) < .7) & (da['nits']<100)
-        da.da['mask'] = ((-da['resid']/abs(da['I0']) < .7) & (da['nits'] < 100)
+        #  da.da['mask']=(da['resid']/abs(da['I0']) < .7) & (da['nits']<100)
+        da.da['mask'] = ((da['resid']/abs(da['I0']) < .7) & (da['nits'] < da['maxits'])
                          & (np.abs(da['Vp']) < 200) & (np.abs(da['Te']) < 200))
         q = 1.602e-19
         mi = 1.67e-27

pyfusion/examples/plot_LP2D.py
@@ -1,3 +1,6 @@
+""" Script to animate Te and ne - first attempt - needs generalising
+"""
+#_PYFUSION_TEST_@@Skip
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.interpolate import griddata
@@ -19,9 +22,13 @@ da = DA('20160302_12_L57')
 Te_range = [10, 100]  # 20160302_12
 ne_range = [0, 2]
 
-da = DA('20160310_9_L53')
-Te_range = [20, 100]  # 20160310_9
-ne_range = [0, 10]
+da = DA('20160302_12_L57')
+Te_range = [10, 100]  # 20160302_12
+ne_range = [0, 2]
+
+da = DA('20160309_42_L53')
+Te_range = [20, 100]  # 
+ne_range = [0, 4]
 
 sc_kwargs = dict(vmin=Te_range[0], vmax=Te_range[1]) if Te_range is not None else {}
 ne_kwargs = dict(vmin=ne_range[0], vmax=ne_range[1]) if ne_range is not None else {}
@@ -31,14 +38,15 @@ if ne_range is not None:
 
 srange = range(100, 200)
 srange = range(180, 185)
+srange = range(67, 70)   # 20160309_42_L53
 #srange = range(110, 111)
 #srange = range(len(da['t_mid']))
-step = 0  # 3  # use every (step) time slice but skip unsuitable ones
+step = 3  # 3  # use every (step) time slice but skip unsuitable ones
 
 st = 0
 skipped = []
 for s in srange:
-    fig = plt.figure(100, figsize=(12, 8))
+    #fig = plt.figure(100, figsize=(12, 8))
     fig = plt.figure(100, figsize=(8, 6))
     ne_raw = da.masked['ne'][s]
     wg = np.where(~np.isnan(ne_raw))[0]
@@ -89,3 +97,4 @@ for s in srange:
         plt.close(fig)
     else:
         plt.show()
+